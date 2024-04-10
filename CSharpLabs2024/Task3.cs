9964e63bb4a1b818bd4de2440d70f16476df7c49
DenGas <dehuc1811@gmail.com>
2024-04-10 05:36:44 +0300

    Added Interface for Task, 3-4 tasks resolved

---
+++ CSharpLabs2024/Task3.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLabs2024
+{
+    public class Task3 : ITaskable
+    {
+        public void Run()
+        {
+            Console.WriteLine("Task3\n");
+
+            ConsoleColor originalColor = Console.ForegroundColor; 
+
+            int arrSize = 0 + 4;
+
+            int[] X = new int[arrSize];
+
+            for (int i = 0; i < X.Length; i++)
+            {
+                Console.Write($"Enter int element X[{i}]: ");
+
+                while (!int.TryParse(Console.ReadLine(), out X[i]))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Please enter a valid int element X[{i}]: ");
+                    Console.ForegroundColor = originalColor; 
+                    Console.Write($"Enter int element X[{i}]: ");
+                }
+            }
+
+            int max = X[0];
+            int min = X[0];
+
+            for (int i = 0; i < arrSize; i++)
+            {
+                if (X[i] > max)
+                {
+                    max = X[i];
+                }
+
+                if (X[i] < min)
+                {
+                    min = X[i];
+                }
+
+            }
+
+
+            Console.Write($"\nMax element in arr {max}, min {min}");
+
+
+        }
+    }
+
+}
