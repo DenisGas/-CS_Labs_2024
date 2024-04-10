b74781e9e93699ed7bc76dad10ba4d104bab6022
DenGas <dehuc1811@gmail.com>
2024-04-10 06:41:41 +0300

    Added solution of 1 and 2 tasks

---
+++ CSharpLabs2024/Task1/Book.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLabs2024.Task1
+{
+    internal class Book
+    {
+        private readonly Title title;
+        private Author author;
+        private Content content;
+
+        public Book(string bookTitle)
+        {
+            title = new Title(bookTitle);
+        }
+
+        public Author Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+
+        public Content Content
+        {
+            get { return content; }
+            set { content = value; }
+        }
+
+        public void Show()
+        {
+            title.Show();
+            author.Show();
+            content.Show();
+        }
+    }
+
+}
---
+++ CSharpLabs2024/Task1/Task.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLabs2024.Task1
+{
+     internal class Task
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Task1");
+
+            Book book = new Book("C# Programming");
+            book.Author = new Author { Name = "Sasha Molodetc" };
+            book.Content = new Content { Text = "This is text about Sasha" };
+
+            book.Show();
+
+        }
+    }
+}
