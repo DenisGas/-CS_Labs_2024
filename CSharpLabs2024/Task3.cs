3bdc4a42049b9db35a5f09813006f614b1c3da1a
DenGas <dehuc1811@gmail.com>
2024-04-11 03:50:18 +0300

    Refactored code

--- CSharpLabs2024/Task3.cs
+++ CSharpLabs2024/Task3.cs
@@ -8,48 +8,68 @@ namespace CSharpLabs2024
 {
     public class Task3 : ITaskable
     {
+        private int GetMininArr(int[] arr)
+        {
+            int min = arr[0];
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+
+            return min;
+        }
+
+        private int GetMaxinArr(int[] arr)
+        {
+            int max = arr[0];
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+
+            return max;
+        }
+
+        private void FillIntArr(int[] arr)
+        {
+
+            ConsoleColor originalColor = Console.ForegroundColor;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"Enter int element X[{i}]: ");
+
+                while (!int.TryParse(Console.ReadLine(), out arr[i]))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Please enter a valid int element X[{i}]: ");
+                    Console.ForegroundColor = originalColor;
+                    Console.Write($"Enter int element X[{i}]: ");
+                }
+            }
+
+        }
+
+
         public void Run()
         {
             Console.WriteLine("Task3\n");
-
-            ConsoleColor originalColor = Console.ForegroundColor; 
 
             int arrSize = 0 + 4;
 
             int[] X = new int[arrSize];
 
-            for (int i = 0; i < X.Length; i++)
-            {
-                Console.Write($"Enter int element X[{i}]: ");
-
-                while (!int.TryParse(Console.ReadLine(), out X[i]))
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine($"Please enter a valid int element X[{i}]: ");
-                    Console.ForegroundColor = originalColor; 
-                    Console.Write($"Enter int element X[{i}]: ");
-                }
-            }
-
-            int max = X[0];
-            int min = X[0];
-
-            for (int i = 0; i < arrSize; i++)
-            {
-                if (X[i] > max)
-                {
-                    max = X[i];
-                }
-
-                if (X[i] < min)
-                {
-                    min = X[i];
-                }
-
-            }
-
-
-            Console.Write($"\nMax element in arr {max}, min {min}");
+            FillIntArr(X);
+          
+            Console.Write($"\nMax element in arr {GetMaxinArr(X)}, min {GetMininArr(X)}");
 
 
         }
